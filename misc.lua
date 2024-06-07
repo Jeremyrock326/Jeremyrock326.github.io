5b2f7bdb7abd3310e9fd53069ae9b8c637e4bd3b
Jeremyrock326 <145816556+Jeremyrock326@users.noreply.github.com>
2024-06-06 21:23:49 -0400

    Create misc.lua
    Signed-off-by: Jeremyrock326 <145816556+Jeremyrock326@users.noreply.github.com>

---
+++ misc.lua
@@ -0,0 +1,16 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jeremyrock326/Jeremyrock326.github.io/main/main.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+
+local settings = {
+   AutoChat_Time = 1,
+   AutoChat_Delay = 1,
+   AutoChat = true,
+}
+ 
+local chatrem = game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest
+ 
+while task.wait(settings.AutoChat_Time) do
+    chatrem:FireServer('Jeremy Hub V1 On Top!', "All")
+    print("Sent Message")
+end
